254a25a66a73ec2bb494e8cef9af4f8ceb917250
GeorgiyMK <georgiymk82@gmail.com>
2024-11-10 21:37:02 +0300

    task 1

--- README.py
+++ README.py
@@ -3,3 +3,27 @@ class Student:
         self.name = name
         self.surname = surname
         self.gender = gender
+        self.finished_courses = []
+        self.courses_in_progress = []
+        self.grades = {}
+
+class Mentor:
+    def __init__(self, name, surname):
+        self.name = name
+        self.surname = surname
+        self.courses_attached = []
+
+class Lecturer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+class Reviewer(Mentor):
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+
+
+# best_student = Student('Ruoy', 'Eman', 'your_gender')
+# best_student.finished_courses += ['Git']
+# best_student.courses_in_progress += ['Python']
+# best_student.grades['Git'] = [10, 10, 10, 10, 10]
+# best_student.grades['Python'] = [10, 10]
