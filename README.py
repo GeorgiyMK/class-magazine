d837495148b44991b392734d0022d759bfbce11c
GeorgiyMK <georgiymk82@gmail.com>
2024-11-11 20:34:56 +0300

    try 1

--- README.py
+++ README.py
@@ -7,6 +7,15 @@ class Student:
         self.courses_in_progress = []
         self.grades = {}
 
+    def rate_lecturer(self, lecturer, course,grade):
+        if isinstance(lecturer, Lecturer) and course in lecturer.courses_attached and course in self.courses_in_progress:
+            if course in lecturer.grades:
+                lecturer.grades[course] +=[grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
+
 class Mentor:
     def __init__(self, name, surname):
         self.name = name
@@ -16,11 +25,21 @@ class Mentor:
 class Lecturer(Mentor):
     def __init__(self, name, surname):
         super().__init__(name, surname)
+        self.grades = {}
 
 class Reviewer(Mentor):
+
     def __init__(self, name, surname):
         super().__init__(name, surname)
 
+    def rate_student(self, student, course, grade):
+        if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
+            if course in student.grades:
+                student.grades[course] += [grade]
+            else:
+                student.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 # best_student = Student('Ruoy', 'Eman', 'your_gender')
 # best_student.finished_courses += ['Git']
